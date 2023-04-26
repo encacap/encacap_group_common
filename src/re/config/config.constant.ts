e36da9ca755a8b06d26e9e8aab77c20341ab4567
Khanh Nguyen <encacap@gmail.com>
2023-04-26 15:58:41 +0700

    fix: SITE_CONFIG_CODE_ENUM

--- src/re/config/config.constant.ts
+++ src/re/config/config.constant.ts
@@ -8,3 +8,12 @@ export enum CONFIG_TYPE_ENUM {
   NUMBER = "number",
   BOOLEAN = "boolean",
 }
+
+export enum SITE_CONFIG_CODE_ENUM {
+  SITE_NAME = "site_name",
+  SITE_PHONE_NUMBER = "site_phone_number",
+  SITE_ZALO = "site_zalo",
+  SITE_ADDRESS = "site_address",
+  SITE_FACEBOOK = "site_facebook",
+  SITE_YOUTUBE = "site_youtube",
+}

6b62c4a959dc03e6587b33dd585af6957e1e5da1
Khanh Nguyen <encacap@gmail.com>
2023-05-15 13:17:25 +0700

    fix: update categoryGroupCode property of the ICategory

--- src/re/category/category.interface.ts
+++ src/re/category/category.interface.ts
@@ -1,11 +1,12 @@
 import { ICloudflareImageResponse } from "../cloudflare";
 import { IWebsite } from "../website";
+import { CATEGORY_GROUP_ENUM } from "./category-group.constant";
 
 export interface ICategory {
   id: number;
   code: string;
   name: string;
-  categoryGroupCode: string;
+  categoryGroupCode: CATEGORY_GROUP_ENUM;
   thumbnail: ICloudflareImageResponse;
   thumbnailId: string;
   websiteId: number;

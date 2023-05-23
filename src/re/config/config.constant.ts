cece9bd9418c5ccb15a3f984bbf8105fdc9b6795
Khanh Nguyen <encacap@gmail.com>
2023-05-23 21:55:18 +0700

    fix: group related config code to CONFIG_CODE_ENUM

--- src/re/config/config.constant.ts
+++ src/re/config/config.constant.ts
@@ -9,20 +9,20 @@ export enum CONFIG_TYPE_ENUM {
   IMAGE_ARRAY = "image_array",
 }
 
-export enum BAOLOCRE_SITE_CONFIG_CODE_ENUM {
+export enum CONFIG_CODE_ENUM {
   PHONE_NUMBER = "site_phone_number",
-  ZALO = "site_zalo",
   ADDRESS = "site_address",
   FACEBOOK = "site_facebook",
-  YOUTUBE = "site_youtube",
   HOMEPAGE_SLIDER_IMAGES = "site_homepage_slider_images",
 }
 
+export enum BAOLOCRE_SITE_CONFIG_CODE_ENUM {
+  ZALO = "site_zalo",
+  YOUTUBE = "site_youtube",
+}
+
 export enum ACBUILDING_SITE_CONFIG_CODE_ENUM {
-  HOMEPAGE_SLIDER_IMAGES = "site_homepage_slider_images",
-  PHONE_NUMBER = "site_phone_number",
   EMAIL = "site_email",
-  ADDRESS = "site_address",
   FAX = "site_fax",
   BANK = "site_bank",
 }

89575f624549814c3512f2d1ace30bc3a85e7ad1
Khanh Nguyen <encacap@gmail.com>
2023-05-24 09:29:10 +0700

    fix: update site config code enum

--- src/re/config/config.constant.ts
+++ src/re/config/config.constant.ts
@@ -7,13 +7,15 @@ export enum CONFIG_TYPE_ENUM {
   PRIMITIVE = "primitive",
   IMAGE = "image",
   IMAGE_ARRAY = "image_array",
+  CONTACT = "contact",
+  ADDRESS = "address",
 }
 
-export enum CONFIG_CODE_ENUM {
+export enum SITE_CONFIG_CODE_ENUM {
   PHONE_NUMBER = "site_phone_number",
   ADDRESS = "site_address",
   FACEBOOK = "site_facebook",
-  HOMEPAGE_SLIDER_IMAGES = "site_homepage_slider_images",
+  HOMEPAGE_SLIDER_IMAGE = "site_homepage_slider_image",
 }
 
 export enum BAOLOCRE_SITE_CONFIG_CODE_ENUM {
@@ -25,4 +27,5 @@ export enum ACBUILDING_SITE_CONFIG_CODE_ENUM {
   EMAIL = "site_email",
   FAX = "site_fax",
   BANK = "site_bank",
+  HOMEPAGE_INTRODUCE_IMAGE = "site_homepage_introduce_image",
 }

28df683e03804a47799a28965ada7e05caf109e8
Khanh Nguyen <encacap@gmail.com>
2023-06-08 16:20:14 +0700

    fix: update WEBSITE_DOMAIN_ENUM

--- src/re/website/website.constant.ts
+++ src/re/website/website.constant.ts
@@ -2,4 +2,7 @@ export enum WEBSITE_DOMAIN_ENUM {
   BAOLOCRE_DEV = "dev.baolocre.re.encacap.com",
   ENCACAP_RE_DEV = "dev.re.encacap.com",
   ACBUILDING_DEV = "dev.acbuilding.re.encacap.com",
+  ENCACAP_RE = "re.encacap.com",
+  ACBUILDING = "acbuilding.re.encacap.com",
+  BAOLOCRE = "baolocre.re.encacap.com",
 }

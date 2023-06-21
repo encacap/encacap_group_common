1aabbad355e10d3d027f8426d0903303a6e7cfa2
Khanh Nguyen <encacap@gmail.com>
2023-06-21 17:24:03 +0700

    fix: update IConfig

--- src/re/config/config.interface.ts
+++ src/re/config/config.interface.ts
@@ -1,11 +1,10 @@
 import { IWebsite } from "../website";
-import { CONFIG_GROUP_ENUM, CONFIG_TYPE_ENUM } from "./config.constant";
+import { CONFIG_TYPE_ENUM } from "./config.constant";
 
 export interface IConfig {
   code: string;
-  group: CONFIG_GROUP_ENUM;
   type: CONFIG_TYPE_ENUM;
   value: string;
-  websiteId?: number;
+  websiteId: number;
   website: IWebsite;
 }

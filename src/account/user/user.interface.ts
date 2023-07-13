23fd657f3743f6841cbd11b757e4d2951fb4090f
Khanh Nguyen <encacap@gmail.com>
2023-07-13 10:42:09 +0700

    fix: add permissions to IUser

--- src/account/user/user.interface.ts
+++ src/account/user/user.interface.ts
@@ -1,3 +1,4 @@
+import { IPermission } from "../permission";
 import { IRole } from "../role";
 
 export interface IUser {
@@ -8,4 +9,5 @@ export interface IUser {
   firstName: string;
   lastName: string;
   roles: IRole[];
+  permissions: IPermission[];
 }
